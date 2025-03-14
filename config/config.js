b909c5db9545684c04ca3b12f156be3e47baea0c
dikshamenghmalani <menghmalani.diksha07@gmail.com>
2025-03-14 01:53:48 -0500

    chore: initial commit

--- config/config.js
+++ config/config.js
@@ -1,169 +1,162 @@
-
-import profile from './profile.png';
-import { faAppStore, faGithub, faGooglePlay } from '@fortawesome/free-brands-svg-icons';
-import { } from '@fortawesome/free-solid-svg-icons';
+import profile from "./profile.jpg";
+import {
+	faAppStore,
+	faGithub,
+	faGooglePlay,
+} from "@fortawesome/free-brands-svg-icons";
+import {} from "@fortawesome/free-solid-svg-icons";
 
 export const navigation = {
-  name: "Hashir",
-  links: [
-    {
-      title: "About",
-      link: "#about",
-    },
-    {
-      title: "Projects",
-      link: "#projects",
-    },
-    {
-      title: "Contact",
-      link: "#contact",
-    },
-    {
-      title: "Links",
-      link: "/links",
-    },
-    {
-      title: "Blog",
-      link: "https://medium.com/@hashirshoaeb",
-    }
-  ],
-}
+	name: "Diksha",
+	links: [
+		{
+			title: "About",
+			link: "#about",
+		},
+		{
+			title: "Projects",
+			link: "#projects",
+		},
+		{
+			title: "Contact",
+			link: "#contact",
+		},
+		{
+			title: "Links",
+			link: "/links",
+		},
+	],
+};
 export const intro = {
-  title: "Hey, I'm Hashir",
-  description: "A Computer Engineer creating mobile apps and static websites.",
-  image: profile.src,
-  buttons: [
-    {
-      title: "Contact Me",
-      link: "#contact",
-      isPrimary: true,
-    },
-    {
-      title: "Resume",
-      link: "https://docs.google.com/document/d/13_PWdhThMr6roxb-UFiJj4YAFOj8e_bv3Vx9UHQdyBQ/edit?usp=sharing",
-      isPrimary: false,
-    },
-  ],
-}
+	title: "Hey, I'm Diksha Menghmalani",
+	description:
+		"Computer Science graduate exploring the frontiers of AI, ML, SWE and data-driven innovation solutions to make a real-world impact",
+	image: profile.src,
+	buttons: [
+		{
+			title: "Contact Me",
+			link: "#contact",
+			isPrimary: true,
+		},
+		{
+			title: "Resume",
+			link: "https://drive.google.com/file/d/1hr1BeSly--Slkq35R4xHY9CdjAjLHCIH/view",
+			isPrimary: false,
+		},
+	],
+};
 
 export const about = {
-  title: "Who I am",
-  description: [
-    "I graduated from National University of Sciences and Technology (NUST) in 2020 with a degree in Computer Engineering. I kick started my professional career as a mobile application developer working remotely for a UK based startup — Lessgo.",
-    "The tech industry is ever-evolving and I love being able to grow with it, while continually solidifying the fundamentals. I opensource my code from a place of empathy - for future developers, teammates, users, and with accessibility in mind. I also love joining communities, helping and mentoring new developers, and supporting organizations to promote diversity in tech. I’m sharing my programming journey on instagram, helping people who are just getting into the space of programming.",
-    "When I’m not programming, I focus on my hobbies which are: diary writing, doodling, meeting people and growing my network.",
-  ],
-}
+	title: "Who I am",
+	description: [
+		"I’m a graduate student in the Department of Computer Science at the University of Wisconsin-Madison, specializing in Data Engineering. My expertise lies in designing scalable data pipelines and leveraging AI to extract meaningful insights from complex datasets. I have experience working with Python, SQL, and PySpark to build efficient solutions. My past work has involved feature engineering, data modeling, automation, and visualization to drive data-driven decision-making.",
+		"I’m deeply interested in AI and ML, particularly in how advanced algorithms can be integrated into real-world applications to make a real-world impact! I love solving challenging technical problems, optimizing performance, and continuously learning new technologies.",
+		"If you’re working on data science, AI, or software development, I’d love to connect and exchange insights!",
+	],
+};
 
 export const work = {
-  title: "What I do",
-  cards: [
-    {
-      title: "Mobile App Development",
-      description: "I create pixel perfect iOS and Andriod apps using Flutter.",
-      icons: null,
-    },
-    {
-      title: "Web Development",
-      description: "I create responsive static websites using Reactjs.",
-      icons: null,
-    }
-  ],
-}
+	title: "What I do",
+	cards: [
+		{
+			title: "Mobile App Development",
+			description: "I create pixel perfect iOS and Andriod apps using Flutter.",
+			icons: null,
+		},
+		{
+			title: "Web Development",
+			description: "I create responsive static websites using Reactjs.",
+			icons: null,
+		},
+	],
+};
 
 export const projects = {
-  title: "Projects",
-  cards: [
-    {
-      title: "StarBook",
-      description: "A digital diary and mood tracking app that helps you in keeping track of your mood and productivity throughout the month/year.",
-      icons: [
-        {
-          icon: faAppStore,
-          link: "https://apps.apple.com/us/app/starbook-journal-ai-diary/id1552418289",
-        },
-        {
-          icon: faGithub,
-          link: "https://github.com/hashirshoaeb/star_book",
-        },
-      ]
-    },
-    {
-      title: "QuranTalk",
-      description: "An emotional well being and mental health app. The app helps people to navigate their emotions in the light of the Quran.",
-      icons: [
-        {
-          icon: faAppStore,
-          link: "https://apps.apple.com/us/app/qurantalk/id1563425149",
-        },
-        {
-          icon: faGooglePlay,
-          link: "https://play.google.com/store/apps/details?id=com.ayahemotion.quran_talk",
-        },
-      ]
-    },
-    {
-      title: "Portfolio",
-      description: "A portfolio website template that helps you showcase your work, projects and skills as a software developer or freelancer.",
-      icons: [
-        {
-          icon: faGithub,
-          link: "https://github.com/hashirshoaeb/portfolio",
-        },
-      ]
-    },
-  ],
-}
+	title: "Projects",
+	cards: [
+		{
+			title: "StarBook",
+			description:
+				"A digital diary and mood tracking app that helps you in keeping track of your mood and productivity throughout the month/year.",
+			icons: [
+				{
+					icon: faAppStore,
+					link: "https://apps.apple.com/us/app/starbook-journal-ai-diary/id1552418289",
+				},
+				{
+					icon: faGithub,
+					link: "https://github.com/hashirshoaeb/star_book",
+				},
+			],
+		},
+		{
+			title: "QuranTalk",
+			description:
+				"An emotional well being and mental health app. The app helps people to navigate their emotions in the light of the Quran.",
+			icons: [
+				{
+					icon: faAppStore,
+					link: "https://apps.apple.com/us/app/qurantalk/id1563425149",
+				},
+				{
+					icon: faGooglePlay,
+					link: "https://play.google.com/store/apps/details?id=com.ayahemotion.quran_talk",
+				},
+			],
+		},
+		{
+			title: "Portfolio",
+			description:
+				"A portfolio website template that helps you showcase your work, projects and skills as a software developer or freelancer.",
+			icons: [
+				{
+					icon: faGithub,
+					link: "https://github.com/hashirshoaeb/portfolio",
+				},
+			],
+		},
+	],
+};
 
 export const contact = {
-  title: "Get in touch",
-  description: "Coffee Chat! Please do not hesitate to schedule a meeting. Alternatively, feel free to reach out directly by email at hashirshoaeb@gmail.com.",
-  buttons: [
-    {
-      title: "Email Me",
-      link: "mailto:hashirshoaeb@gmail.com",
-      isPrimary: true,
-    },
-    {
-      title: "Schedule Meeting",
-      link: "https://topmate.io/hashirshoaeb",
-      isPrimary: false,
-    },
-  ]
-}
+	title: "Get in touch",
+	description:
+		"Feel free to reach out directly by email at menghmalani@wisc.edu to connect and exchange insights.",
 
-// SEARCH ENGINE 
+	buttons: [
+		{
+			title: "Email Me",
+			link: "mailto:menghmalani@wisc.edu",
+			isPrimary: true,
+		},
+	],
+};
+
+// SEARCH ENGINE
 export const SEO = {
-  // 50 - 60 char  
-  title: "Hashir Shoaib | Computer Engineer | Flutter | Reactjs developer",
-  description: "I create mobile apps and static websites. I graduated from National University of Sciences and Technology (NUST) in 2020 with a degree in Computer Engineering.",
-  image: profile.src,
-}
+	// 50 - 60 char
+	title: "Diksha Menghmalani | Computer Engineer | Reactjs developer",
+	description:
+		"I create mobile apps and static websites. I graduated from National University of Sciences and Technology (NUST) in 2020 with a degree in Computer Engineering.",
+	image: profile.src,
+};
 
 export const links = {
-  image: profile.src,
-  title: "@hashirshoaeb",
-  description: "Computer Engineer | Flutter | Reactjs Developer",
-  cards: [
-    {
-      title: "My website",
-      link: "https://hashirshoaeb.com/",
-    },
-    {
-      title: "QuranTalk App",
-      link: "https://www.qurantalk.app/",
-    },
-    {
-      title: "StarBook App",
-      link: "https://starbook.dev/",
-    },
-    {
-      title: "My GitHub",
-      link: "https://github.com/hashirshoaeb/",
-    },
-    {
-      title: "My LinkedIn",
-      link: "https://www.linkedin.com/in/hashirshoaeb/",
-    },
-  ]
-}
+	image: profile.src,
+	title: "@hashirshoaeb",
+	description: "Computer Engineer | Flutter | Reactjs Developer",
+	cards: [
+		{
+			title: "Portfolio Website",
+			link: "https://hashirshoaeb.com/",
+		},
+		{
+			title: "My GitHub",
+			link: "https://github.com/dikshamenghmalani",
+		},
+		{
+			title: "My LinkedIn",
+			link: "https://www.linkedin.com/in/diksha-menghmalani/",
+		},
+	],
+};
